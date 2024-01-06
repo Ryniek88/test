d17afa4acb36fcde292ece710fa2426c87902279
Ryniek88 <adam_r88@wp.pl>
2024-01-06 13:27:21 +0100

    new commit

---
+++ src/Garnek.java
@@ -0,0 +1,16 @@
+public class Garnek {
+
+        int high;
+        int width;
+        String color;
+
+       public Garnek(){
+       }
+       public Garnek (int high, int width, String color){
+            this.high = high;
+            this.width = width;
+            this.color = color;
+        }
+
+
+}
