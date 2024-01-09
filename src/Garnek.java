1fb604e17d3cd7ba38080ca90d0bc721d543ac3b
Ryniek88 <adam_r88@wp.pl>
2024-01-09 14:39:52 +0100

    new commit

--- src/Garnek.java
+++ src/Garnek.java
@@ -4,7 +4,9 @@ public class Garnek {
         int width;
         String color;
 
-       public Garnek(){
+       public Garnek(int high,int width){
+           this.high=high;
+           this.width=width;
        }
        public Garnek (int high, int width, String color){
             this.high = high;
@@ -12,5 +14,15 @@ public class Garnek {
             this.color = color;
         }
 
+        public String gotuj (){
+           return "Gotowanie w trakcie";
+        }
+        public String gotuj (boolean czydodalessol){
+           if (czydodalessol){
+               return "Gotowanie z sola";
+           } else {
+               return "Gototowanie bez soli";
+           }
+        }
 
 }
